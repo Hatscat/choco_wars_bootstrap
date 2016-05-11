b8040fc833753ebf0d232c1e5f7c56dd892eb9f9
Diana Boudy <diana.boudy@gmail.com>
2016-05-11 15:51:42 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -1,38 +1,7 @@
 "use strict"
 
-onload = main;
+$(document).ready(function () {
 
-function main () {
-    window.buffer_canvas = visible_canvas.cloneNode();
-    window.visible_ctx = get_2d_ctx(visible_canvas);
-    window.ctx = get_2d_ctx(buffer_canvas);
-    window.time = 0;
-    window.on_resize_cbs = [];
-    window.on_update_cbs = [];
-    on_resize();
-    window.mouse = {
-        x: 0,
-        y: 0,
-        is_down: false,
-        on_down_cbs: [],
-        on_up_cbs: [],
-        on_move_cbs: []
-    };
-    window.gvalues = { 4:0, 5: 10000 };
-    window.struct = get_structure();
 
-    storage.load();
-    init_events();
-    loop(time);
-}
-
-function loop (t) {
-    window.deltatime = t - time;
-    time = t % 0xfffffff;
-
-    on_update();
-    update();
-    draw();
-    requestAnimationFrame(loop);
-}
+});
 
--- tabs/strategy.js
+++ tabs/strategy.js
@@ -3,18 +3,22 @@
 $(document).ready(function () {
 
 	$("#prodInput").slider({ id: "prodSlider", min: 0, max: 1000, value: 0, tooltip: 'always' })
-	$("#priceInput").slider({ id: "priceSlider", min: 0, max: 1000, value: 0, tooltip: 'always' })
+	$("#priceInput").slider({ id: "priceSlider", min: 0, max: 100, value: 0, tooltip: 'always' })
+	$("#promoInput").slider({ id: "promoSlider", min: 0, max: 1000, value: 0, tooltip: 'always' })
 
 	$("#prodInput").on("slide", on_prod_slide);
 	$("#priceInput").on("slide", on_price_slide);
-    addEventListener("resize", on_resize, false);
+	$("#promoInput").on("slide", on_promo_slide);
+    //addEventListener("resize", on_resize, false);
     on_resize();
 });
 
 
 function on_resize () {
-	$("#prodSlider").css( { width: innerWidth * 0.6 } );
-	$("#priceSlider").css( { width: innerWidth * 0.6 } );
+    var slider_w = innerWidth * 0.6;
+	$("#prodSlider").css( { width: slider_w } );
+	$("#priceSlider").css( { width: slider_w } );
+	$("#promoSlider").css( { width: slider_w } );
 }
 
 function on_prod_slide (evnt) {
@@ -24,3 +28,8 @@ function on_prod_slide (evnt) {
 function on_price_slide (evnt) {
     //$("#priceValue").text(evnt.value);
 }
+
+function on_promo_slide (evnt) {
+    //$("#priceValue").text(evnt.value);
+}
+
