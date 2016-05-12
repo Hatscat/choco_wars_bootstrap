2a30ee4bd8e767a654a98da09a1f6166718b36b2
Diana Boudy <diana.boudy@gmail.com>
2016-05-12 18:04:25 +0200

    fix districts

--- tabs/strategy.js
+++ tabs/strategy.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     window.prod_val = 0;
     window.price_val = 0;
     window.promo_val = 0;
+    window.dt1_val = 0;
+    window.dt2_val = 0;
+    window.dt3_val = 0;
     window.place_val = 0;
     window.current_fin_val = 0;
     window.total_cost = 0;
@@ -14,9 +17,9 @@ $(document).ready(function () {
 	$("#priceInput").slider({ id: "priceSlider", min: 0, max: 100, value: price_val, tooltip: 'always' })
 	$("#promoInput").slider({ id: "promoSlider", min: 0, max: 1000, value: promo_val, tooltip: 'always' })
 
-	$("#prodInput").on("slide", on_prod_slide);
-	$("#priceInput").on("slide", on_price_slide);
-	$("#promoInput").on("slide", on_promo_slide);
+	$("#prodInput").on("slide", on_slide.bind(null, "prod_val"));
+	$("#priceInput").on("slide", on_slide.bind(null, "price_val"));
+	$("#promoInput").on("slide", on_slide.bind(null, "promo_val"));
     addEventListener("resize", on_resize, false);
 	$("#mapBt").on("click", on_map_button_click);
 
@@ -32,6 +35,7 @@ function on_resize () {
 }
 
 function update_values () {
+    place_val = dt1_val * 50 + dt2_val * 150 + dt3_val * 350; // todo
     total_cost = prod_val + promo_val + place_val;
     next_fin_val = current_fin_val - total_cost;
     $("#placeValDom").text(place_val);
@@ -39,18 +43,8 @@ function update_values () {
     $("#nextFinValDom").text(next_fin_val);
 }
 
-function on_prod_slide (evnt) {
-    prod_val = +evnt.value;
-    update_values();
-}
-
-function on_price_slide (evnt) {
-    price_val = +evnt.value;
-    update_values();
-}
-
-function on_promo_slide (evnt) {
-    promo_val = +evnt.value;
+function on_slide (var_name, evnt) {
+    window[var_name] = +evnt.value;
     update_values();
 }
 
@@ -63,7 +57,7 @@ function on_map_button_click () {
 function init_map () {
 
     mapCanvas.width = 898 - 30;
-    mapCanvas.height = window.innerHeight * 0.77;
+    mapCanvas.height = window.innerHeight * 0.78;
     window.mapCtx = mapCanvas.getContext("2d");
 
     mapCtx.fillRect(0, 0, mapCanvas.width, mapCanvas.height);
@@ -81,6 +75,7 @@ function init_map () {
     // dt1
 	$("#dt1Input").slider({ id: "dt1Slider", min: 0, max: 10, value: prod_val, tooltip: 'always' })
 	$("#dt1Slider").css( { width: slider_w } );
+	$("#dt1Input").on("slide", on_slide.bind(null, "dt1_val"));
     new Chart(dt1Canvas, {
         type: 'pie',
         data: new_chart_data(dt_people_tastes_labels, dt_people_tastes_colors, [16, 48, 36])
@@ -89,6 +84,7 @@ function init_map () {
     // dt2
 	$("#dt2Input").slider({ id: "dt2Slider", min: 0, max: 10, value: prod_val, tooltip: 'always' })
 	$("#dt2Slider").css( { width: slider_w } );
+	$("#dt2Input").on("slide", on_slide.bind(null, "dt2_val"));
     new Chart(dt2Canvas, {
         type: 'pie',
         data: new_chart_data(dt_people_tastes_labels, dt_people_tastes_colors, [27, 29, 44])
@@ -97,6 +93,7 @@ function init_map () {
     // dt3
 	$("#dt3Input").slider({ id: "dt3Slider", min: 0, max: 10, value: prod_val, tooltip: 'always' })
 	$("#dt3Slider").css( { width: slider_w } );
+	$("#dt3Input").on("slide", on_slide.bind(null, "dt3_val"));
     new Chart(dt3Canvas, {
         type: 'pie',
         data: new_chart_data(dt_people_tastes_labels, dt_people_tastes_colors, [53, 9, 38])
